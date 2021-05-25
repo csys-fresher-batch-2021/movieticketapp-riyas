670311fbc6c64adf673016e66ba8216d7b299c8d
Riyas Ahamed U <Riyas21052@gmail.com>
2021-05-25 18:10:38 +0530

    Added JSON for Seat Details and Add Movie Feature

---
+++ src/main/java/in/riyasahamed/servlets/AddMovieServletJSON.java
@@ -0,0 +1,51 @@
+package in.riyasahamed.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.JsonObject;
+
+import in.riyasahamed.service.MovieService;
+
+/**
+ * Servlet implementation class AddMovieServletJSON
+ */
+@WebServlet("/AddMovieServletJSON")
+public class AddMovieServletJSON extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+    public AddMovieServletJSON() {
+        super();
+        //Default Constructor
+    }
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String movieName=request.getParameter("movieName");
+		String actorName=request.getParameter("actorName");
+		PrintWriter out = response.getWriter();
+		try {
+			Float rating=Float.parseFloat(request.getParameter("rating"));
+			MovieService.addMovieDetails(movieName, actorName, rating);
+			JsonObject object = new JsonObject();
+			object.addProperty("infoMessage", "Successfully Added");
+			out.println(object); 	
+		} catch (Exception e) {
+			e.printStackTrace();
+			JsonObject object = new JsonObject();
+			object.addProperty("errorMessage", e.getMessage());
+			out.println(object);
+		}
+		out.flush();
+		
+	}
+
+}
