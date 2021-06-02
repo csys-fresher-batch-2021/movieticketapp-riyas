39a0e0f0b4a5cf20ae3549ea8f7d12fee3fb1355
Riyas Ahamed U <Riyas21052@gmail.com>
2021-06-02 11:53:24 +0530

    Fixed Build Error

--- src/test/java/in/riyasahamed/service/ShowTimesTest.java
+++ src/test/java/in/riyasahamed/service/ShowTimesTest.java
@@ -3,7 +3,7 @@ package in.riyasahamed.service;
 import java.time.LocalTime;
 import java.util.List;
 
-public class Test {
+public class ShowTimesTest {
 
 	public static void main(String[] args) {
 		List<LocalTime> showTimes = TicketService.getShowTimes();
